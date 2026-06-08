eae2f54a8f10389bb5fbe84995f57e280389b4f3
Raphael Oliveira <rapahaeru@gmail.com>
2026-06-08 17:00:08 -0300

    Implements DTO validations

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
